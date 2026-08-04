b3d5e5f566521a5a359d3ff92778e019e53570b1
Sangita Ghosal <sangitaghosal018@gmail.com>
2026-08-05 00:01:43 +0530

    Create remove_duplicates.py

---
+++ remove_duplicates.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        if len(nums) == 0:  
+          return 0
+        l = 0
+        r = 1
+
+        while r<len(nums):
+            if nums[l] != nums[r]:
+              l = l+1
+              nums[l] = nums[r]
+            r = r+1
+        return l+1
